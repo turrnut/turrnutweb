b1d6346285061ef091b305b758127ad158f3c240
turrnut <victorconqueror021@gmail.com>
2024-11-25 16:48:40 -0500

    Adding templates

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    print("Someone visited the home page")
+    return render_template(f"index.html")
+
+@app.errorhandler(500)
+def internal_error(error):
+    return "500"
+
+app.run()
