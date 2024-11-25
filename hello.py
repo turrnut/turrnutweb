2aedbf855bcb4efb03cc566fa08c75e6e05adea0
turrnut <victorconqueror021@gmail.com>
2024-11-25 17:10:01 -0500

    Test Verce

--- hello.py
+++ hello.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    print("Someone visited the home page")
     return render_template(f"index.html")
 
 @app.errorhandler(500)
